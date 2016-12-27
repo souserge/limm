3f0e843b5b7b1feee32fc31653bd42a669046d5e
qw8 <serge.korzh@gmail.com>
2016-12-27 01:34:05 +0100

    init commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var cols, rows, scl;
+var pixelGrid;
+
+function preload() {
+
+}
+
+function setup() {
+  createCanvas(windowHeight, windowHeight);
+  restartSketch();
+}
+
+function restartSketch() {
+}
+
+function draw() {
+  background(30);
+}
