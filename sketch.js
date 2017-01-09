4863c03806a7d710405118feb105f27d96e1540a
qw8 <serge.korzh@gmail.com>
2017-01-09 13:48:01 +0100

    center canvas

--- sketch.js
+++ sketch.js
@@ -1,15 +1,27 @@
 //import {Mover} from "./classes/mover.js";
 //import {Wall} from "./classes/wall.js";
 let map  = [];
+let cnv;
 
 function preload() { // will need it for loading textures, sounds and other files
 
 }
 
 function setup() {
-  createCanvas(WID, HEI);
+  cnv = createCanvas(WID, HEI);
+  centerCanvas();
   restartSketch();
   textFont("Courier New");
+}
+
+function centerCanvas() {
+  let x = (windowWidth - width) / 2;
+  let y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+function windowResized() {
+  centerCanvas();
 }
 
 function restartSketch() {
